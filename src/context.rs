0469bba967446358d8509a98ebb5ef955f70301c
cykna <kina.lima1302@proton.me>
2025-12-05 15:18:24 -0300

    feat: added support for showing the sourcefile with where the error was caused

--- src/context.rs
+++ src/context.rs
@@ -1,6 +1,6 @@
 use std::{collections::HashMap, path::PathBuf, sync::Arc};
 
-use color_eyre::eyre::Result;
+use color_eyre::{eyre::Result, owo_colors::OwoColorize};
 
 use crate::parser::lexer::{Lexer, error::LexerError};
 
@@ -18,23 +18,34 @@ pub enum SlynxErrorType {
 pub struct SlynxError {
     ty: SlynxErrorType,
     line: usize,
-    column: usize,
+    column_start: usize,
+    column_end: usize,
     message: String,
+    ///The file path the error occuried
+    file: String,
+    source: String,
 }
 impl std::error::Error for SlynxError {}
 
 impl std::fmt::Display for SlynxError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         let type_error = match self.ty {
-            SlynxErrorType::Lexer => "Lexing",
-            SlynxErrorType::Parser => "Parsing",
-            SlynxErrorType::Compilation => "Compilation",
-            SlynxErrorType::Type => "Type ",
+            SlynxErrorType::Lexer => "Lexing Error",
+            SlynxErrorType::Parser => "Parsing Error",
+            SlynxErrorType::Compilation => "Compilation Error",
+            SlynxErrorType::Type => "Type Checking Error",
         };
+        let mut err = " ".repeat(self.column_end);
+        err.replace_range(self.column_start - 1..self.column_end, "^");
         writeln!(
             f,
-            "{type_error} Error: {} at line: {}:{}",
-            self.message, self.line, self.column
+            "{} at file {} on line: {}:{}: {}{}",
+            type_error.red(),
+            self.file.yellow(),
+            self.line.blue(),
+            self.column_start.blue(),
+            self.message.red(),
+            format!("{}\n{err}", self.source,)
         )
     }
 }
@@ -82,21 +93,33 @@ impl SlynxContext {
     }
 
     ///Based on the provided `index`, which is the index of a char on the source code of `path`, returns the line where it's located on the file of the provided `path`.
-    ///This will return its line and the column
-    pub fn get_line_info(&self, path: &Arc<PathBuf>, index: usize) -> (usize, usize) {
+    ///This will return its line and the column and the line containing the error
+    pub fn get_line_info(&self, path: &Arc<PathBuf>, index: usize) -> (usize, usize, &str) {
         let lines = self
             .lines
             .get(path)
             .expect("Path should be provided on the context");
+        let source = self
+            .files
+            .get(path)
+            .expect("Path should be provided on the context");
         let out = match lines.binary_search(&index) {
-            Ok(v) => (v, index - lines[v]),
-            Err(e) => (e + 1, {
-                let mut column = index.saturating_sub(lines[e.saturating_sub(1)]);
-                if column == 0 {
-                    column = index + 1;
-                }
-                column
-            }),
+            Ok(v) => (
+                v,
+                index - lines[v],
+                &source[lines[index - lines[v]]..lines[v + 1]],
+            ),
+            Err(e) => (
+                e + 1,
+                {
+                    let mut column = index.saturating_sub(lines[e.saturating_sub(1)]);
+                    if column == 0 {
+                        column = index + 1;
+                    }
+                    column
+                },
+                &source[lines[e - 1]..lines[e]],
+            ),
         };
 
         out
@@ -107,12 +130,15 @@ impl SlynxContext {
             Ok(value) => value,
             Err(e) => match e {
                 LexerError::UnrecognizedChar { index, .. } => {
-                    let (line, column) = self.get_line_info(&self.entry_point, index);
+                    let (line, column, src) = self.get_line_info(&self.entry_point, index);
                     return Err(SlynxError {
                         line,
                         ty: SlynxErrorType::Lexer,
-                        column,
+                        column_start: column,
+                        column_end: column,
                         message: e.to_string(),
+                        file: self.entry_point.to_string_lossy().to_string(),
+                        source: src.to_string(),
                     }
                     .into());
                 }
