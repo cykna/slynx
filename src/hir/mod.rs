2806dd45411d507a4cbeda235b15f78001b820dc
cykna <kina.lima1302@proton.me>
2025-12-30 21:40:52 -0300

    removal: removed macros from the hir and their resolution

--- src/hir/mod.rs
+++ src/hir/mod.rs
@@ -1,7 +1,6 @@
 pub mod declaration;
 pub mod error;
 mod implementation;
-pub mod macros;
 mod scope;
 pub mod types;
 use std::{
@@ -16,7 +15,7 @@ use crate::{
             HirStatmentKind,
         },
         error::{HIRError, HIRErrorKind},
-        macros::{DeclarationMacro, ElementMacro, StatmentMacro},
+
         scope::HIRScope,
         types::HirType,
     },
@@ -394,68 +393,5 @@ impl SlynxHir {
             }
         }
         Ok(())
-    }
-    ///Expands, or atleast, tries to, the given AST if it's a macro call declaration
-    fn expand_decl(
-        &mut self,
-        idx: usize,
-        ast: &ASTDeclaration,
-    ) -> Result<Option<Vec<ASTDeclaration>>, HIRError> {
-        match &ast.kind {
-            ASTDeclarationKind::MacroCall(call_data) => {
-                let macr = self
-                    .decl_macros
-                    .get::<&str>(&call_data.name.as_ref())
-                    .ok_or(HIRError {
-                        kind: HIRErrorKind::NameNotRecognized(call_data.name.clone()),
-                        span: ast.span.clone(),
-                    })?;
-                Ok(Some(macr.execute(&call_data.args, idx)))
-            }
-            _ => Ok(None),
-        }
-    }
-    ///Expands, or atleast, tries to, the given AST if it's a macro call statment
-    fn expand_element(
-        &mut self,
-        idx: usize,
-        ast: &ElementDeffinition,
-    ) -> Result<Option<Vec<ElementDeffinition>>, HIRError> {
-        match &ast.kind {
-            ElementDeffinitionKind::MacroCall { name, args } => {
-                let macr = self
-                    .elmt_macros
-                    .get::<&str>(&name.as_ref())
-                    .ok_or(HIRError {
-                        kind: HIRErrorKind::NameNotRecognized(name.clone()),
-                        span: Span {
-                            start: ast.span.start,
-                            end: ast.span.start + name.len(),
-                        },
-                    })?;
-                Ok(Some(macr.execute(&args, idx)))
-            }
-            _ => Ok(None),
-        }
-    }
-    ///Expands, or atleast, tries to, the given AST if it's a macro call statment
-    fn expand_stmt(
-        &mut self,
-        idx: usize,
-        ast: &ASTStatment,
-    ) -> Result<Option<Vec<ASTStatment>>, HIRError> {
-        match &ast.kind {
-            ASTStatmentKind::MacroCall(call_data) => {
-                let macr = self
-                    .stmt_macros
-                    .get::<&str>(&call_data.name.as_ref())
-                    .ok_or(HIRError {
-                        kind: HIRErrorKind::NameNotRecognized(call_data.name.clone()),
-                        span: ast.span.clone(),
-                    })?;
-                Ok(Some(macr.execute(&call_data.args, idx)))
-            }
-            _ => Ok(None),
-        }
     }
 }
