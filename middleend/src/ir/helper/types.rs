85e4c770244e8147ff063b905b687fe961b16c0c
caina <kina.lima1302@proton.me>
2026-03-30 12:07:15 -0300

    chore: moved functions to make code more concise
    feat: initialized components

--- middleend/src/ir/helper/types.rs
+++ middleend/src/ir/helper/types.rs
@@ -66,7 +66,6 @@ impl SlynxIR {
             Value::Instruction(instr) => self.get_type_of_instruction(instr.clone(), temp),
             Value::LabelArg(_) => unimplemented!("Unimplemented type for label args"),
             Value::Slot(v) => self.get_slot_type(v.clone()),
-            Value::StructLiteral(ir, _) => *ir,
         }
     }
 
@@ -148,30 +147,6 @@ impl SlynxIR {
         let ty = self.types.get_function_type_mut(func_tyid);
         ty.insert_arg_types(&extended_args);
         ty.set_return_type(ret);
-        Ok(())
-    }
-    /// Inserts the contents of the provided `decl` into an IR struct type asserting it's a slynx component. This is made because component can be lowered to the equivalent of a struct with methods, thus 'classes'.
-    /// The thing is that this is made interanlly with the minimum of abstraction as possible, so it becomes a struct and the components as well as methods are inserted directly into the struct as fields
-    pub(crate) fn insert_component_fields_for(
-        &mut self,
-        decl: TypeId,
-        temp: &mut TempIRData,
-        tys: &TypesModule,
-    ) -> Result<(), IRError> {
-        let component_type = self.get_ir_type(&decl, temp, tys)?;
-        let IRType::Component(cid) = self.types.get_type(component_type) else {
-            unreachable!("Something errored that type of component simply isnt Component on ir");
-        };
-
-        let Some(HirType::Component { props: ty_props }) = tys.get_component(&decl) else {
-            unreachable!("{:?} should map to an Component, but it doesn't", decl);
-        };
-
-        for (_, _, prop) in ty_props {
-            let ty = self.get_ir_type(prop, temp, tys)?;
-            let comp_ty = self.types.get_component_type_mut(cid);
-            comp_ty.insert_field(ty);
-        }
         Ok(())
     }
 }
--- middleend/src/ir/model/instruction.rs
+++ middleend/src/ir/model/instruction.rs
@@ -15,7 +15,6 @@ pub struct Slot {
 #[derive(Debug, Clone)]
 ///A value inside the IR. Can be a function arg, a label arg or the result of a instruction
 pub enum Value {
-    StructLiteral(IRTypeId, IRPointer<Value>),
     Raw(IRPointer<Operand, 1>),
     Instruction(IRPointer<Instruction, 1>),
     Slot(IRPointer<Slot, 1>),
@@ -37,6 +36,7 @@ pub enum Operand {
 pub enum InstructionType {
     ///Variant used for raw values. Their actual value is their operand
     RawValue,
+    Struct,
     ///Variant used for function calls. The `func` field is the pointer to the function context
     FunctionCall(IRPointer<Context, 1>),
     ///Variant used for binary add. The type is determines by the `value_type` and the left and right hand side are the `operands`
@@ -297,6 +297,14 @@ impl Instruction {
         Self {
             operands: value.with_length(),
             instruction_type: InstructionType::Read,
+            value_type: ty,
+        }
+    }
+
+    pub fn struct_literal(ty: IRTypeId, value: IRPointer<Value>) -> Self {
+        Self {
+            operands: value,
+            instruction_type: InstructionType::Struct,
             value_type: ty,
         }
     }
