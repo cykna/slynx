4426151982d8b6b5bb735ca1ca94660765d04e39
LuisOtavio13 <roberdoogarcia@gmail.com>
2026-04-12 16:59:15 -0300

    refactor(statement.rs)

--- frontend/src/checker/statement.rs
+++ frontend/src/checker/statement.rs
@@ -19,31 +19,46 @@ impl TypeChecker {
         statement: &mut HirStatement,
         expected: &TypeId,
     ) -> Result<()> {
+
         match &mut statement.kind {
+
             HirStatementKind::Variable { name, value } => {
                 // Ensure the initializer expression is fully typed and propagate it to the variable.
                 self.default_expr(value)?;
+
                 let ty = self.get_type_of_expr(value)?;
+
                 value.ty = ty;
                 self.types_module.insert_variable(*name, ty);
+
             }
 
             HirStatementKind::Assign { lhs, value } => {
+
                 let ty = self.resolve(&lhs.ty, &lhs.span)?;
                 value.ty = self.unify(&ty, &value.ty, &value.span)?;
+
             }
+
             HirStatementKind::Expression { expr } => self.default_expr(expr)?,
+
             HirStatementKind::Return { expr } => {
+
                 self.default_expr(expr)?;
                 let unify = self.unify(&expr.ty, expected, &statement.span)?;
+
                 expr.ty = unify;
             }
 
             HirStatementKind::While { condition, body } => {
+
                 condition.ty = self.get_type_of_expr(condition)?;
                 let bool_id = self.types_module.bool_id();
+
                 condition.ty = self.unify(&condition.ty, &bool_id, &condition.span)?;
+
                 for stmt in body {
+
                     self.default_statement(stmt, expected)?;
                 }
             }
