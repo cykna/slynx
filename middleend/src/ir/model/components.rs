bd3336bf4cea02060fe287efaa7f047dd1e193a3
caina <kina.lima1302@proton.me>
2026-04-14 12:47:30 -0300

    chore: removed unused field on component

--- middleend/src/ir/model/components.rs
+++ middleend/src/ir/model/components.rs
@@ -3,14 +3,14 @@ use crate::{IRPointer, IRTypeId, Value};
 #[derive(Debug, Clone)]
 pub struct Component {
     pub(crate) ty: IRTypeId,
-    pub(crate) values: IRPointer<Value>,
+    pub(crate) _values: IRPointer<Value>,
 }
 
 impl Component {
     pub fn new(initial_type: IRTypeId, values: IRPointer<Value>) -> Self {
         Self {
             ty: initial_type,
-            values,
+            _values: values,
         }
     }
 }
