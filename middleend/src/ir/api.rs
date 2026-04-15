5b56d44bfbf5d2f377910cb23967c9e29d9aac8d
caina <kina.lima1302@proton.me>
2026-04-15 14:28:03 -0300

    feat: added functions to retreive values and instruction by ptr

--- middleend/src/ir/api.rs
+++ middleend/src/ir/api.rs
@@ -1,4 +1,4 @@
-use crate::{Context, IRPointer, IRTypes, Instruction, Label, Operand, SlynxIR};
+use crate::{Context, IRPointer, IRTypes, Instruction, Label, Operand, SlynxIR, Value};
 
 impl SlynxIR {
     pub fn contexts(&self) -> &[Context] {
@@ -20,8 +20,17 @@ impl SlynxIR {
         &self.types
     }
 
-    ///Retrieves the `Operands` pointed by the given ptr
+    ///Retrieves a `operand` array that is pointed by the given ptr
     pub fn get_operand_by_pointer(&self, ptr: IRPointer<Operand>) -> &[Operand] {
         &self.operands[ptr.range()]
+    }
+    ///Retrieves a `value` array that is pointed by the given ptr
+    pub fn get_values_by_pointer(&self, ptr: IRPointer<Instruction>) -> &[Value] {
+        &self.values[ptr.range()]
+    }
+
+    ///Retrieves a `instruction` array that is pointed by the given ptr
+    pub fn get_instructions_by_pointer(&self, ptr: IRPointer<Instruction>) -> &[Instruction] {
+        &self.instructions[ptr.range()]
     }
 }
